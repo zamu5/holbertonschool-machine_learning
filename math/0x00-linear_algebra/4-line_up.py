c4a0a122ed923430c078cfe3d5f8591f4dd54ab5
Sergio Zamudio <szamu5@hotmail.com>
2020-12-07 09:17:49 -0500

    points 2-7

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" function to add two arrays elements-wise"""
+
+
+def add_arrays(arr1, arr2):
+    """ adding two arrays element wise
+    Args:
+        arr1, arr2: Given arrays
+    Return:
+        the sum of arrays: new matrix
+    """
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        return [sum(x) for x in zip(arr1, arr2)]
