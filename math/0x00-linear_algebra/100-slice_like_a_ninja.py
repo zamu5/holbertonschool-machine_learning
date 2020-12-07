b6cb26c53d83dffc7beb1bf6a868ce0d4d2e2b24
Sergio Zamudio <szamu5@hotmail.com>
2020-12-07 09:36:22 -0500

    points advance

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""function to slice matrix along specific values"""
+
+
+def np_slice(matrix, axes={}):
+    """ slice matrix along specific value
+    Args:
+        matrix: Given matrix
+    Return:
+        the slice mat: slice_mat
+    """
+    slice_mat = [slice(None, None, None)] * matrix.ndim
+    for k, v in sorted(axes.items()):
+        slice_val = slice(*v)
+        slice_mat[k] = slice_val
+    matrix = matrix[tuple(slice_mat)]
+    return matrix
