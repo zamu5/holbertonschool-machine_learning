1026c44d96ebaba82abc154778e0d4324f58ef0f
Sergio Zamudio <szamu5@hotmail.com>
2020-12-10 09:33:48 -0500

    0x02-calculus - all points

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function that sum powers based on Faulhaber's Formula"""
+
+
+def summation_i_squared(n):
+    """m(m+1)(2m+1) / 6"""
+
+    if type(n) is not int:
+        return None
+    elif n < 1:
+        return None
+    else:
+        return int((n * (1 + n)) * (2 * n + 1) / 6)
